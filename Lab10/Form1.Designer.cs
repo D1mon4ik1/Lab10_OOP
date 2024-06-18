ba331bdf0c5a50c6b692457812d310c9286e2748
D1mon4ik1 <128784535+D1mon4ik1@users.noreply.github.com>
2024-06-18 17:45:38 +0300

    lab10

---
+++ Lab10/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace Lab10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 285);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(241, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Запустити 1 Потік";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(278, 285);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(241, 30);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Запустити 2 Потік";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(547, 285);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(241, 30);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Запустити 3 Потік";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.ForeColor = System.Drawing.Color.Red;
+            this.button4.Location = new System.Drawing.Point(12, 321);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(241, 30);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Зупинити 1 Потік";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.ForeColor = System.Drawing.Color.Red;
+            this.button5.Location = new System.Drawing.Point(278, 321);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(241, 30);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Зупинити 2 Потік";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.ForeColor = System.Drawing.Color.Red;
+            this.button6.Location = new System.Drawing.Point(547, 321);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(241, 30);
+            this.button6.TabIndex = 3;
+            this.button6.Text = "Зупинити 3 Потік";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(12, 373);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(776, 30);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "Запустити всі потоки";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.ForeColor = System.Drawing.Color.Red;
+            this.button8.Location = new System.Drawing.Point(12, 409);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(776, 30);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "Зупинити всі потоки";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(241, 256);
+            this.richTextBox1.TabIndex = 10;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(278, 12);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(241, 256);
+            this.richTextBox2.TabIndex = 11;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(547, 12);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(241, 256);
+            this.richTextBox3.TabIndex = 12;
+            this.richTextBox3.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Location = new System.Drawing.Point(15, 15);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "3 потокова програма";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+
+        private System.Windows.Forms.Button button1;
+
+        #endregion
+    }
+}
---
+++ Lab10/Form1.cs
@@ -0,0 +1,317 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Digests;
+using Org.BouncyCastle.Crypto.Engines;
+using Org.BouncyCastle.Crypto.Modes;
+using Org.BouncyCastle.Crypto.Paddings;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Security;
+
+namespace Lab10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            thread1 = new Thread(new ThreadStart(CastEncryption));
+            thread2 = new Thread(new ThreadStart(Md4Hashing));
+            thread3 = new Thread(new ThreadStart(SealEncryption));
+        }
+        
+        Thread thread1;
+        Thread thread2;
+        Thread thread3;
+        
+        private volatile bool thread1Running = false;
+        private volatile bool thread2Running = false;
+        private volatile bool thread3Running = false;
+        
+        private void CastEncryption()
+        {
+            try
+            {
+                var engine = new Cast5Engine();
+                var key = new byte[16]; // 128-bit key
+                new SecureRandom().NextBytes(key);
+                var keyParam = new KeyParameter(key);
+
+                var cipher = new PaddedBufferedBlockCipher(new CbcBlockCipher(engine));
+                cipher.Init(true, new ParametersWithIV(keyParam, new byte[8]));
+
+                var plaintext = Encoding.UTF8.GetBytes("This is a test message");
+                var ciphertext = new byte[cipher.GetOutputSize(plaintext.Length)];
+                var length = cipher.ProcessBytes(plaintext, 0, plaintext.Length, ciphertext, 0);
+                cipher.DoFinal(ciphertext, length);
+
+                richTextBox1.Invoke((MethodInvoker)delegate ()
+                {
+                    richTextBox1.Text = BitConverter.ToString(ciphertext).Replace("-", "");
+                });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                thread1Running = false;
+            }
+        }
+
+        private void Md4Hashing()
+        {
+            try
+            {
+                var digest = new MD4Digest();
+                var input = Encoding.UTF8.GetBytes("This is a test message");
+                digest.BlockUpdate(input, 0, input.Length);
+
+                var output = new byte[digest.GetDigestSize()];
+                digest.DoFinal(output, 0);
+
+                richTextBox2.Invoke((MethodInvoker)delegate ()
+                {
+                    richTextBox2.Text = BitConverter.ToString(output).Replace("-", "");
+                });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                thread2Running = false;
+            }
+        }
+
+        private void SealEncryption()
+        {
+            try
+            {
+                var engine = new SealEngine();
+                var key = new byte[16]; // 128-bit key
+                new SecureRandom().NextBytes(key);
+                var keyParam = new KeyParameter(key);
+
+                engine.Init(true, keyParam);
+
+                var plaintext = Encoding.UTF8.GetBytes("This is a test message");
+                var ciphertext = new byte[plaintext.Length];
+                engine.ProcessBytes(plaintext, 0, plaintext.Length, ciphertext, 0);
+
+                richTextBox3.Invoke((MethodInvoker)delegate ()
+                {
+                    richTextBox3.Text = BitConverter.ToString(ciphertext).Replace("-", "");
+                });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                thread3Running = false;
+            }
+        }
+
+        public class SealEngine
+        {
+            private uint[] S = new uint[512];
+            private uint[] X = new uint[4];
+            private int counter = 0;
+
+            public void Init(bool forEncryption, ICipherParameters parameters)
+            {
+                KeyParameter keyParam = (KeyParameter)parameters;
+                byte[] key = keyParam.GetKey();
+                InitializeS(key);
+                InitializeX(key);
+            }
+
+            public void ProcessBytes(byte[] input, int inOff, int length, byte[] output, int outOff)
+            {
+                for (int i = 0; i < length; i++)
+                {
+                    if (counter == 0)
+                    {
+                        GenerateNextBlock();
+                    }
+                    output[outOff + i] = (byte)(input[inOff + i] ^ S[counter]);
+                    counter = (counter + 1) & 0x1FF;
+                }
+            }
+
+            private void InitializeS(byte[] key)
+            {
+                for (int i = 0; i < 256; i++)
+                {
+                    S[i] = BitConverter.ToUInt32(key, (i * 4) % key.Length);
+                }
+                for (int i = 256; i < 512; i++)
+                {
+                    S[i] = S[i - 256];
+                }
+            }
+
+            private void InitializeX(byte[] key)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    X[i] = BitConverter.ToUInt32(key, (i * 4) % key.Length);
+                }
+            }
+
+            private void GenerateNextBlock()
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    X[i] += S[(X[(i + 1) & 3] >> 10) & 0x1FF];
+                    S[(X[i] >> 10) & 0x1FF] = X[i];
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!thread1Running)
+                {
+                    thread1 = new Thread(new ThreadStart(CastEncryption));
+                    thread1.Start();
+                    thread1Running = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!thread2Running)
+                {
+                    thread2 = new Thread(new ThreadStart(Md4Hashing));
+                    thread2.Start();
+                    thread2Running = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!thread3Running)
+                {
+                    thread3 = new Thread(new ThreadStart(SealEncryption));
+                    thread3.Start();
+                    thread3Running = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                thread1.Suspend();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                thread2.Suspend();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                thread3.Suspend();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!thread1Running)
+                {
+                    thread1 = new Thread(new ThreadStart(CastEncryption));
+                    thread1.Start();
+                    thread1Running = true;
+                }
+
+                if (!thread2Running)
+                {
+                    thread2 = new Thread(new ThreadStart(Md4Hashing));
+                    thread2.Start();
+                    thread2Running = true;
+                }
+
+                if (!thread3Running)
+                {
+                    thread3 = new Thread(new ThreadStart(SealEncryption));
+                    thread3.Start();
+                    thread3Running = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                thread1.Suspend();
+                thread2.Suspend();
+                thread3.Suspend();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            thread1.Abort();
+            thread2.Abort();
+            thread3.Abort();
+        }
+    }
+}
